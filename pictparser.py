b1c4601cf55900f2d6a29fd8dfa6f2029a64d95e
Reid Ellis <rae@tnir.org>
2017-03-23 14:07:33 -0400

    Handles more than one file, cuts off comments that can confuse the regexp

--- pictparser.py
+++ pictparser.py
@@ -4,44 +4,51 @@ import os
 import re
 import struct
 import sys
-import subprocess
+from subprocess import *
 
 if len(sys.argv) < 2:
-    print "Usage: "+sys.argv[0]+": <path to file>"
-    sys.exit(1)
+	print "Usage: "+sys.argv[0]+": file [file ..]"
+	sys.exit(1)
 
-p = subprocess.Popen(["DeRez", "-only", "PICT", sys.argv[1]], \
-			stdout=subprocess.PIPE,\
-			stderr=subprocess.PIPE)
-out, err = p.communicate()
-out = out.split('\n')
-for l in out:
-    if len(l) == 1:
-        continue
-    # start of file
-    if re.match('data \'PICT\'', l):
-        m = re.search('\(\d*\)', l)
-        num = m.group(0)
-        num = num.translate(None, '()')
-        print "open "+num
-        working = open("res"+num+".pct", "wb")
-        for a in range(0, 512):
-            s = struct.pack(">B", 0);
-            working.write(s)
-    # end of file
-    elif re.match('\}\;', l):
-        working.close()
-        print "done"
-    # file data
-    else:
-        cap = re.search('\".*\" ', l)
-        if cap is None:
-            continue
-        data = cap.group(0)
-        data = data.translate(None, '"')
-        for a in data.split(' '):
-            if len(a) != 4:
-                continue
-            first, second = a[:len(a)/2], a[len(a)/2:]
-            pdata = struct.pack(">BB", int(first, 16), int(second, 16))
-            working.write(pdata)
+def extract(arg):
+	# build output file name
+	outputName = os.path.splitext(arg)[0] + "_df.pct"
+	p = Popen(["DeRez", "-only", "PICT", arg], stdout=PIPE, stderr=PIPE)
+	out, err = p.communicate()
+	out = out.split('\n')
+	for l in out:
+		if len(l) == 1:
+			continue
+		# start of file
+		if re.match('data \'PICT\'', l):
+			m = re.search('\(\d*\)', l)
+			num = m.group(0)
+			num = num.translate(None, '()')
+			working = open(outputName, "wb")
+			print "\"%s\" > \"%s\"" % (arg, outputName)
+			for a in range(0, 512):
+				s = struct.pack(">B", 0);
+				working.write(s)
+		# end of file
+		elif re.match('\}\;', l):
+			working.close()
+		# file data
+		else:
+			# avoid errors trying to parse comments
+			if len(l) > 48:
+				l = l[:48]
+			cap = re.search('\".*\" ', l)
+			if cap is None:
+				continue
+			data = cap.group(0)
+			data = data.translate(None, '"')
+			for a in data.split(' '):
+				if len(a) != 4:
+					continue
+				first, second = a[:len(a)/2], a[len(a)/2:]
+				# print "first=%s, second=%s" % (first, second)
+				pdata = struct.pack(">BB", int(first, 16), int(second, 16))
+				working.write(pdata)
+
+for arg in sys.argv[1:]:
+	extract(arg)
